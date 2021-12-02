49bf322f13c31e24991f1da8204b784037de7877
Alex Lue <a.lue820@gmail.com>
2021-12-02 15:04:26 -0800

    1 refactor

--- 1/one.py
+++ 1/one.py
@@ -2,21 +2,19 @@ with open('1/input.txt') as f:
     depths = f.read().split('\n')
     depths = [int(depth) for depth in depths]
 
-
-def part_1():
+def depth_comparison(win):
     increased = 0
-    for i in range(1, len(depths)):
-        if depths[i-1] < depths[i]:
+    for i in range(win, len(depths)):
+        if depths[i-win] < depths[i]:
             increased += 1
     return increased
+
+def part_1():
+    return depth_comparison(1)
 
 
 def part_2():
-    increased = 0
-    for i in range(3, len(depths)):
-        if depths[i-3] < depths[i]:
-            increased += 1
-    return increased
+    return depth_comparison(3)
 
 print(part_1())
 print(part_2())
